635715b238cd7972cff4ee7a0b759f5eb2db5bf2
joelin109 <joe.lin1009@gmail.com>
2017-02-21 09:38:06 +0800

    render to html file

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"server-api-go/src/service/api"
+	"net/http"
 )
 
 func main() {
 	// initialize gin
 	router := gin.Default()
-
 	api.RegisterRoutes(router)
 
-	router.Run(":8088")
+	router.StaticFile("asset/css/styles6.css", "./www/asset/css/styles6.css")
+	router.StaticFile("app.bundle.js", "./www/app.bundle.js")
+	router.LoadHTMLFiles("www/index.html")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.Run(":8095")
+
 }
